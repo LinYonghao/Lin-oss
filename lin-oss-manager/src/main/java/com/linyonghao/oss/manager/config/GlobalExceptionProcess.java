8b9431c30a5bc9dd448f4705805ff08fac8b1440
林永浩 <goodhao926@163.com>
2022-06-28 11:15:50 +0800

    1.添加权限无效页面

--- lin-oss-manager/src/main/java/com/linyonghao/oss/manager/config/GlobalExceptionProcess.java
+++ lin-oss-manager/src/main/java/com/linyonghao/oss/manager/config/GlobalExceptionProcess.java
@@ -2,6 +2,7 @@ package com.linyonghao.oss.manager.config;
 
 
 import cn.dev33.satoken.exception.NotLoginException;
+import cn.dev33.satoken.exception.NotRoleException;
 import com.linyonghao.oss.manager.utils.ResponseUtil;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 @ControllerAdvice
 public class GlobalExceptionProcess {
 
+    @ExceptionHandler(NotRoleException.class)
+    public ModelAndView notRoleProcess(HttpServletResponse httpServletResponse, Exception e){
+        return ResponseUtil.view("exception/403",null);
+    }
     @ExceptionHandler(NotLoginException.class)
     public ModelAndView notLoginProcess(HttpServletResponse httpServletResponse, Exception e){
         return ResponseUtil.error("forward:/user/login",null,"请先登录哦,宝!");
